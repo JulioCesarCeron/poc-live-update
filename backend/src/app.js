6b1203711ce6e3a97c453d825d658030e84e5e7e
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-07-10 20:26:51 -0300

    backend

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = 3200;
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io');
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+})
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/test', function(req, res) {
+  const response = {
+    id: 1,
+    description: 'API test',
+    date: Date.now(),
+  };
+
+  return res.json(response);
+});
+
+app.listen(PORT, function() {
+  console.log(`poc it's a live on port ${PORT}!!`);
+})
+
