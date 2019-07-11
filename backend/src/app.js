a68026f28215973a2f7fe33af54024e7810f1354
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-07-10 21:53:50 -0300

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,26 +5,27 @@ const PORT = 3200;
 
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io');
+const io = require('socket.io')(server);
 
-app.use(cors());
-app.use(express.json());
 
 app.use((req, res, next) => {
   req.io = io;
   return next();
 })
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.get('/test', function(req, res) {
+app.get('/version', function(req, res) {
   const response = {
     id: 1,
     date: Date.now(),
     description: 'API test',
-    verion: '0.0.0',
+    version: '0.0.0',
   };
 
   return res.json(response);
