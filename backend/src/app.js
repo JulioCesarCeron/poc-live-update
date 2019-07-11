870042d60fe842d23dc58cd9b1292302bf5d61dd
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-07-10 22:25:04 -0300

    add routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,32 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 app.use((req, res, next) => {
+  console.log('req', req);
   req.io = io;
   return next();
 })
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/version', function(req, res) {
-  const response = {
-    id: 1,
-    date: Date.now(),
-    description: 'API test',
-    version: '0.0.0',
-  };
-  
-  return res.json(response);
-});
+app.use(require('./routes'))
 
 const PORT = 3000;
 app.listen(PORT, () => {
