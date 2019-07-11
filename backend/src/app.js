3421e86d16135ff0298e7cc34ffe6ad4b3510121
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-07-10 23:58:41 -0300

    fix socket.io connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,36 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const cors = require('cors');
 
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use((req, res, next) => {
-  console.log('req', req);
-  req.io = io;
-  return next();
-})
+
+io.on('connection', socket => {
+  console.log(`Socket conectado ${socket.id}`);
+  socket.emit('news', { hello: 'world' });
+  
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log("user disconnected");
+  })
+});
 
 app.use(cors());
 app.use(express.json());
-app.use(require('./routes'))
+app.use(require('./routes'));
 
-const PORT = 3000;
-app.listen(PORT, () => {
+
+
+const PORT = 3200;
+server.listen(PORT, () => {
   console.log(`poc it's a live on port ${PORT}!!`);
-})
+});
 
+module.exports = io;
